18039371277c72971d6fb9687669a4a738960e81
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-10 15:04:22 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,8 @@
+export const item = {
+    "id": "1870f904-b3dd-411e-85dd-97b6b10cb598",
+    "turn_on": "sense",
+    "notification": "black",
+    "view_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/136.png",
+    "about_reminders": "History also condition system. Sound born help pull.",
+    "maybe_later": "Campaign available history. Individual property run technology author like wall. Off this few only his save indicate."
+}
